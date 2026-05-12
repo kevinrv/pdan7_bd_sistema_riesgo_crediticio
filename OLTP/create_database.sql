9c46a9b97785c241d9d381fa915cdcab006380db
Kevin Rivera <kevinriveravergaray@gmail.com>
2026-05-11 21:40:34 -0500

    Add tables: clientes

--- OLTP/create_database.sql
+++ OLTP/create_database.sql
@@ -35,6 +35,5 @@ cliente_id INT UNIQUE NOT NULL,
 CONSTRAINT fk_clientes_persona_natural FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
 
-
-CREATE TABLE personas_juridicas(     id INT IDENTITY(1,1) PRIMARY KEY,     ruc VARCHAR(11) UNIQUE NOT NULL,     razon_social VARCHAR(255) NOT NULL,     nombre_comercial VARCHAR(255) NULL,     tipo_empresa VARCHAR(100) NOT NULL, 	representante_legal VARCHAR(200) not null,    sector_economico VARCHAR(100) NULL,     direccion VARCHAR(255) NOT NULL,     ubigeo CHAR(6) NULL,     telefono VARCHAR(20) NULL,     correo VARCHAR(255) NULL,     fecha_constitucion DATE NOT NULL,     estado_empresa VARCHAR(50) NOT NULL, 	inicio_actividades DATE NOT NULL,	numero_empleados INT NOT NULL,    cliente_id INT UNIQUE NOT NULL,     CONSTRAINT fk_clientes_persona_juridica     FOREIGN KEY (cliente_id) REFERENCES clientes(id) );
+CREATE TABLE personas_juridicas( id INT IDENTITY(1,1) PRIMARY KEY, ruc VARCHAR(11) UNIQUE NOT NULL, razon_social VARCHAR(255) NOT NULL, nombre_comercial VARCHAR(255) NULL, tipo_empresa VARCHAR(100) NOT NULL, representante_legal VARCHAR(200) NOT NULL,sector_economico VARCHAR(100) NULL, direccion VARCHAR(255) NOT NULL, ubigeo CHAR(6) NULL, telefono VARCHAR(20) NULL, correo VARCHAR(255) NULL, fecha_constitucion DATE NOT NULL, estado_empresa VARCHAR(50) NOT NULL, inicio_actividades DATE NOT NULL,numero_empleados INT NOT NULL,cliente_id INT UNIQUE NOT NULL, CONSTRAINT fk_clientes_persona_juridica FOREIGN KEY (cliente_id) REFERENCES clientes(id) );
 
