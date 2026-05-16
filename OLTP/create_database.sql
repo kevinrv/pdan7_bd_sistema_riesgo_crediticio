9bea675b2a87f4a15e320f83794d440081febd30
Kevin Rivera <kevinriveravergaray@gmail.com>
2026-05-15 21:29:15 -0500

    Add script BD OLTP complete

--- OLTP/create_database.sql
+++ OLTP/create_database.sql
@@ -3,6 +3,10 @@
 CREATE DATABASE pdan_bd_sistema_riesgo_crediticio;
 GO
 
+--- Opcional eliminacion de Base de Datos
+
+---DROP DATABASE pdan_bd_sistema_riesgo_crediticio;
+
 --- Seleccionar base de datos
 USE pdan_bd_sistema_riesgo_crediticio;
 GO
@@ -36,7 +40,26 @@ CONSTRAINT fk_clientes_persona_natural FOREIGN KEY (cliente_id) REFERENCES clien
 );
 
 -- Personas juridicas
-CREATE TABLE personas_juridicas( id INT IDENTITY(1,1) PRIMARY KEY, ruc VARCHAR(11) UNIQUE NOT NULL, razon_social VARCHAR(255) NOT NULL, nombre_comercial VARCHAR(255) NULL, tipo_empresa VARCHAR(100) NOT NULL, representante_legal VARCHAR(200) NOT NULL,sector_economico VARCHAR(100) NULL, direccion VARCHAR(255) NOT NULL, ubigeo CHAR(6) NULL, telefono VARCHAR(20) NULL, correo VARCHAR(255) NULL, fecha_constitucion DATE NOT NULL, estado_empresa VARCHAR(50) NOT NULL,	inicio_actividades DATE NOT NULL,numero_empleados INT NULL,cliente_id INT UNIQUE NOT NULL, CONSTRAINT fk_clientes_persona_juridica FOREIGN KEY (cliente_id) REFERENCES clientes(id) );
+CREATE TABLE personas_juridicas( 
+id INT IDENTITY(1,1) PRIMARY KEY, 
+ruc VARCHAR(11) UNIQUE NOT NULL, 
+razon_social VARCHAR(255) NOT NULL, 
+nombre_comercial VARCHAR(255) NULL, 
+tipo_empresa VARCHAR(100) NOT NULL, 
+representante_legal VARCHAR(200) NOT NULL,
+sector_economico VARCHAR(100) NULL, 
+direccion VARCHAR(255) NOT NULL, 
+ubigeo CHAR(6) NULL, 
+telefono VARCHAR(20) NULL, 
+correo VARCHAR(255) NULL, 
+fecha_constitucion DATE NOT NULL, 
+estado_empresa VARCHAR(50) NOT NULL,	
+inicio_actividades DATE NOT NULL,
+numero_empleados INT NULL,
+cliente_id INT UNIQUE NOT NULL, 
+CONSTRAINT fk_clientes_persona_juridica 
+FOREIGN KEY (cliente_id) REFERENCES clientes(id) 
+);
 
 -- Tipos_cuenta
 
@@ -54,7 +77,7 @@ cci VARCHAR(55) UNIQUE NOT NULL,
 num_tarjeta VARCHAR(30) UNIQUE NOT NULL,
 fecha_creacion DATETIME NOT NULL,
 moneda VARCHAR(50) NOT NULL,
-saldo MONEY NOT NULL,
+saldo DECIMAL(18,2) NOT NULL,
 tipo_cuenta_id INT NOT NULL,
 CONSTRAINT fk_tipos_cuenta FOREIGN KEY (tipo_cuenta_id) REFERENCES tipos_cuenta(id)
 );
@@ -74,8 +97,8 @@ CONSTRAINT fk_cuentas_cliente FOREIGN KEY (cuenta_id) REFERENCES cuentas(id)
 CREATE TABLE productos_crediticios (
 id INT IDENTITY(1,1) PRIMARY KEY,
 nombre VARCHAR(155) UNIQUE NOT NULL,
-monto_minimo MONEY NOT NULL,
-monto_maximo MONEY NOT NULL,
+monto_minimo DECIMAL(18,2) NOT NULL,
+monto_maximo DECIMAL(18,2) NOT NULL,
 tasa_interes_minima DECIMAL(9,2) NOT NULL,
 tasa_interes_maxima DECIMAL(9,2) NOT NULL,
 plazo_minimo_meses INT NOT NULL,
@@ -110,11 +133,127 @@ CREATE TABLE evaluaciones_crediticias
 solicitud_id INT NOT NULL, 
 score_riesgo DECIMAL(10,2) NOT NULL,
 nivel_endeudamiento DECIMAL(10,2) NOT NULL,
-deuda_activa MONEY NOT NULL,
-deuda_activa_otras_entidades MONEY NOT NULL, 
-linea_credito MONEY NOT NULL, 
-linea_credito_otras_entidades MONEY NOT NULL,
-valor_patrimonio MONEY NOT NULL, 
-ingresos_mensuales MONEY NOT NULL,
+deuda_activa DECIMAL(18,2) NOT NULL,
+deuda_activa_otras_entidades DECIMAL(18,2) NOT NULL, 
+linea_credito DECIMAL(18,2) NOT NULL, 
+linea_credito_otras_entidades DECIMAL(18,2) NOT NULL,
+valor_patrimonio DECIMAL(18,2) NOT NULL, 
+ingresos_mensuales DECIMAL(18,2) NOT NULL,
 resultado VARCHAR(100) NOT NULL,
-CONSTRAINT FK_evaluaciones_solicitudes        FOREIGN KEY (solicitud_id)        REFERENCES solicitudes(id));-- creditosCREATE TABLE creditos(    id INT IDENTITY(1,1) PRIMARY KEY,    evaluacion_crediticia_id INT UNIQUE NOT NULL,    monto MONEY NOT NULL,    plazo_meses INT NOT NULL,    tea DECIMAL(10,2) NOT NULL,    tcea DECIMAL(10,2) NOT NULL,    valor_cuota MONEY NOT NULL,    fecha_inicio DATE NOT NULL,    fecha_fin DATE NOT NULL,    fecha_desembolso DATETIME NOT NULL,    numero_credito INT UNIQUE NOT NULL ,    fecha_vencimiento DATE NOT NULL,    estado VARCHAR(100) NOT NULL,    saldo_credito MONEY NOT NULL,    desgravamen MONEY NOT NULL,    CONSTRAINT FK_creditos        FOREIGN KEY (evaluacion_crediticia_id)        REFERENCES evaluaciones_crediticias(id));--cuotasCREATE TABLE cuotas (    id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,    credito_id INT NOT NULL,    num_cuota INT NOT NULL,    fecha_vencimiento DATETIME NOT NULL,    capital MONEY NOT NULL,    intereses MONEY NOT NULL,    seguros MONEY NOT NULL,    total_cuota MONEY NOT NULL,    estado VARCHAR(30) NOT NULL,    tasa_mora DECIMAL(10,2) NOT NULL,    saldo_cuota MONEY NOT NULL,    CONSTRAINT FK_cuotas_creditos FOREIGN KEY (credito_id) REFERENCES creditos(id),	CONSTRAINT chk_total_cuota CHECK (total_cuota=(capital+intereses+seguros)),	CONSTRAINT chk_saldo_cuota CHECK(saldo_cuota<=(capital+intereses+seguros)));-- PagosCREATE TABLE pagos (    id INT IDENTITY(1,1) PRIMARY KEY,    num_operacion VARCHAR(50) UNIQUE,    monto DECIMAL(18,2),    fecha_pago DATETIME,    metodo_pago VARCHAR(50),    observaciones  VARCHAR(200));-- detalle_cuotas_pagosCREATE TABLE detalle_cuotas_pagos(	id INT IDENTITY(1,1) PRIMARY KEY,	cuota_id INT NOT NULL,	pago_id INT NOT NULL,	monto_pagado MONEY NOT NULL,	FOREIGN KEY (cuota_id) REFERENCES cuotas(id),	FOREIGN KEY (pago_id) REFERENCES pagos(id));
+CONSTRAINT FK_evaluaciones_solicitudes
+        FOREIGN KEY (solicitud_id)
+        REFERENCES solicitudes(id)
+);
+
+-- creditos
+CREATE TABLE creditos
+(    id INT IDENTITY(1,1) PRIMARY KEY,
+    evaluacion_crediticia_id INT UNIQUE NOT NULL,
+    monto DECIMAL(18,2) NOT NULL,
+    plazo_meses INT NOT NULL,
+    tea DECIMAL(10,2) NOT NULL,
+    tcea DECIMAL(10,2) NOT NULL,
+    valor_cuota DECIMAL(18,2) NOT NULL,
+    fecha_inicio DATE NOT NULL,
+    fecha_fin DATE NOT NULL,
+    fecha_desembolso DATETIME NOT NULL,
+    numero_credito INT UNIQUE NOT NULL ,
+    fecha_vencimiento DATE NOT NULL,
+    estado VARCHAR(100) NOT NULL,
+    saldo_credito DECIMAL(18,2) NOT NULL,
+    desgravamen DECIMAL(18,2) NOT NULL,
+    CONSTRAINT FK_creditos
+        FOREIGN KEY (evaluacion_crediticia_id)
+        REFERENCES evaluaciones_crediticias(id));
+
+--cuotas
+CREATE TABLE cuotas (
+    id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    credito_id INT NOT NULL,
+    num_cuota INT NOT NULL,
+    fecha_vencimiento DATETIME NOT NULL,
+    capital DECIMAL(18,2) NOT NULL,
+    intereses DECIMAL(18,2) NOT NULL,
+    seguros DECIMAL(18,2) NOT NULL,
+    total_cuota DECIMAL(18,2) NOT NULL,
+    estado VARCHAR(30) NOT NULL,
+    tasa_mora DECIMAL(10,2) NOT NULL,
+    saldo_cuota DECIMAL(18,2) NOT NULL,
+    CONSTRAINT FK_cuotas_creditos FOREIGN KEY (credito_id) REFERENCES creditos(id),
+	CONSTRAINT chk_total_cuota CHECK (total_cuota=(capital+intereses+seguros)),
+	CONSTRAINT chk_saldo_cuota CHECK(saldo_cuota<=(capital+intereses+seguros))
+);
+
+-- Pagos
+
+CREATE TABLE pagos (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    num_operacion VARCHAR(50) UNIQUE,
+    monto DECIMAL(18,2),
+    fecha_pago DATETIME,
+    metodo_pago VARCHAR(50),
+    observaciones  VARCHAR(200)
+);
+
+-- detalle_cuotas_pagos
+
+CREATE TABLE detalle_cuotas_pagos(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	cuota_id INT NOT NULL,
+	pago_id INT NOT NULL,
+	monto_pagado DECIMAL(18,2) NOT NULL,
+	FOREIGN KEY (cuota_id) REFERENCES cuotas(id),
+	FOREIGN KEY (pago_id) REFERENCES pagos(id)
+);
+
+-- Ajustes BD
+
+-- Relacion entre cuentas y clientes
+
+ALTER TABLE cuentas_clientes
+ADD CONSTRAINT UQ_cuentas_clientes UNIQUE(cliente_id, cuenta_id);
+-- Opcional en caso se requiera eliminar la restriccion.
+--ALTER TABLE personas_juridicas
+--DROP CONSTRAINT CK__personas___estad__06CD04F7;
+
+-- Instruccion pára ver los detalles de una tabla
+EXEC SP_HELP cuentas;
+
+--- Agregación de check Para campos que podrían ser tablas maestras
+
+ALTER TABLE personas_naturales
+ADD CHECK (genero IN ('M','F'));
+
+ALTER TABLE personas_naturales
+ADD CHECK (estado_civil IN('S','C','D'));
+
+ALTER TABLE personas_naturales
+ADD CHECK (situacion_laboral IN('Empleado','Independiente','Desempleado', 'No consigna'));
+
+ALTER TABLE personas_juridicas
+ADD CHECK (tipo_empresa IN ('SA','SAC','SRL','EIRL','SAA'));
+
+ALTER TABLE personas_juridicas
+ADD CHECK (estado_empresa IN ('Activo','Inactivo','Suspendido', 'En liquidación'));
+
+ALTER TABLE clientes
+ADD CHECK (tipo_cliente IN ('N','J'));
+
+ALTER TABLE cuentas
+ADD CHECK (moneda IN ('PEN', 'USD', 'EUR'));
+
+ALTER TABLE cuotas
+ADD CHECK (estado IN('pagada','pendiente','pagada parcialmente','refinanciada'));
+
+ALTER TABLE creditos
+ADD CHECK (estado IN ('desembolsado','vigente','Cancelado','refinanciado'));
+
+ALTER TABLE solicitudes
+ADD CHECK (estado IN ('ingresado','aprobada','desestimado','en evaluacion'));
+
+ALTER TABLE productos_crediticios
+ADD CHECK (estado IN ('activo','inactivo','implementandose'));
+
+
+ALTER TABLE cuotas
+ADD CONSTRAINT UQ_credito_num_cuota UNIQUE(credito_id, num_cuota);
