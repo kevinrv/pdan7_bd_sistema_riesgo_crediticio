53fbe0b9740c5754b05b137357b12b459bb57d2f
Kevin Rivera <kevinriveravergaray@gmail.com>
2026-05-25 22:05:51 -0500

    Examples Baqsic and intermedio

--- OLTP/examples_basic.sql
+++ OLTP/examples_basic.sql
@@ -1,11 +1,88 @@
+USE pdan_bd_sistema_riesgo_crediticio;
+GO
 --Nivel Básico (10 preguntas)
 --Mostrar todos los clientes naturales con sus nombres completos y situación laboral.
+
+SELECT
+CONCAT(nt.apellido_paterno,' ', nt.apellido_paterno, ' ', nt.nombres) AS 'nombre_completo',
+nt.situacion_laboral
+FROM personas_naturales nt;
+
 --Mostrar todas las empresas activas y su sector económico.
+
+SELECT razon_social, sector_economicoFROM personas_juridicasWHERE estado_empresa='Activo';
+
+
 --Listar todas las cuentas en moneda PEN.
+
+SELECT num_cuenta
+FROM cuentas
+WHERE moneda='PEN';
+
 --Mostrar todas las solicitudes ingresadas durante el último año.
+
+select codigo_solicitudfrom solicitudeswhere convert(date,fecha_solicitud) between '2025-01-01' and '2025-12-31';
+
+SELECT     codigo_solicitud,    fecha_solicitud,    monto_solicitado,    moneda_solicitada,    estadoFROM     solicitudesWHERE     fecha_solicitud >= '2025/05/25'ORDER BY     fecha_solicitud DESC;
+
+--- Solicitudes del ultimo año
+SELECT *
+FROM solicitudes
+WHERE YEAR(fecha_solicitud)=YEAR(GETDATE());
+
+--- Solicitudes de los ultimos 365(1 año) dias
+SELECT *
+FROM solicitudes
+WHERE fecha_solicitud>=DATEADD(DAY,-365,GETDATE());
+
+
 --Listar todos los productos crediticios activos.
+
+select nombrefrom productos_crediticioswhere estado='ACTIVO'
 --Mostrar todos los créditos con estado vigente.
+
+select id, numero_creditofrom creditoswhere estado='VIGENTE'
+
+
 --Mostrar todas las cuotas pendientes.
+select *from cuotaswhere estado ='pendiente'
+
 --Mostrar todos los pagos realizados mediante transferencia.
+
+
+select *from pagoswhere metodo_pago ='Transferencia'
+
 --Mostrar el listado de clientes junto con el tipo de cliente (N o J).
+
+
+SELECT
+CONCAT(nt.apellido_paterno,' ', nt.apellido_paterno, ' ', nt.nombres) AS 'nombre_completo',
+c.tipo_cliente
+FROM personas_naturales nt
+INNER JOIN clientes c ON nt.cliente_id=c.id AND c.tipo_cliente='N'
+UNION
+SELECT
+razon_social AS 'nombre_completo',
+c.tipo_cliente
+FROM personas_juridicas pj
+INNER JOIN clientes c ON pj.cliente_id=c.id AND c.tipo_cliente='J'
+ORDER BY 2 DESC;
+
+SELECT c.id,
+CASE 
+WHEN pj.razon_social IS NULL 
+	THEN CONCAT(nt.apellido_paterno,' ', nt.apellido_paterno, ' ', nt.nombres)
+ELSE pj.razon_social END AS 'cliente',
+c.tipo_cliente
+FROM clientes c
+LEFT JOIN personas_naturales nt ON nt.cliente_id=c.id AND c.tipo_cliente='N'
+LEFT JOIN personas_juridicas pj ON pj.cliente_id=c.id AND c.tipo_cliente='J'
+ORDER BY 2 DESC;
+
+SELECT*FROM clientes WHERE id IN (36,26,16);
+
+SELECT*FROM personas_juridicas WHERE id IN(16,36)
+SELECT*FROM personas_naturales WHERE id IN(26)
 --Mostrar las cuentas creadas ordenadas por saldo de mayor a menor.
+
+Select * from cuentasorder by saldo desc
---
+++ OLTP/examples_intermedio.sql
@@ -0,0 +1,35 @@
+USE pdan_bd_sistema_riesgo_crediticio;
+GO
+
+--Contar cuántos clientes existen por tipo de cliente.
+SELECT
+	CASE WHEN tipo_cliente = 'J' 
+		THEN 'Cliente Juridico'
+		ELSE 'Cliente Persona Natural' 
+	END AS 'Tipo cliente', 
+	COUNT(*) AS 'Num_clientes'
+FROM clientes
+GROUP BY tipo_cliente;
+
+--Mostrar cuántas cuentas tiene cada cliente.
+
+SELECT  	cliente.id,	CASE 
+	WHEN pj.razon_social IS NULL 
+		THEN CONCAT(nt.apellido_paterno,' ', nt.apellido_paterno, ' ', nt.nombres)
+	ELSE pj.razon_social END AS 'cliente',    cliente.tipo_cliente,    COUNT(cc.cuenta_id) AS cantidad_cuentasFROM clientes clienteLEFT JOIN cuentas_clientes cc ON cliente.id = cc.cliente_idLEFT JOIN personas_naturales nt ON nt.cliente_id=cliente.id AND cliente.tipo_cliente='N'
+LEFT JOIN personas_juridicas pj ON pj.cliente_id=cliente.id AND cliente.tipo_cliente='J'GROUP BY 	cliente.id,	cliente.tipo_cliente,	pj.razon_social,	nt.apellido_paterno, 	nt.apellido_paterno,	nt.nombresORDER BY cantidad_cuentas DESC;
+
+--Calcular el saldo total por moneda.
+Select moneda, sum(saldo) as saldo_total from cuentasgroup by moneda
+--Mostrar el monto promedio solicitado por producto crediticio.
+
+
+
+--Mostrar cuántas solicitudes existen por estado.
+--Obtener el promedio del score de riesgo.
+--Mostrar el total desembolsado por estado de crédito.
+--Mostrar el número de cuotas pendientes por crédito.
+--Mostrar el total pagado por método de pago.
+--Mostrar el promedio de ingresos mensuales según resultado de evaluación.
+--Mostrar cuántos créditos tiene cada cliente.
+--Mostrar los 5 clientes con más cuentas.
